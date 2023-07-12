8a5fa67647950793d8c31fa4338091e01bb20f29
Slug <adamloppe6@gmail.com>
2023-07-12 15:12:38 -0400

    fixs

--- server/server.js
+++ server/server.js
@@ -26,7 +26,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
